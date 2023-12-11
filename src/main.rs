40c6c07445759e64d2c91aa417999429ffdda77d
AnnsAnn <git@annsann.eu>
2023-12-11 22:23:40 +0100

    Fix self routing being transmitted

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use listener::Listener;
 use morganite::Morganite;
 
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::Mutex;
 use std::{collections::HashMap};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::join;
 
 mod header;
 mod routing;
@@ -21,6 +21,7 @@ use routing::{Routingtable};
 use arg_parsing::{parse_name, parse_port};
 
 const LISTEN_ADDR: &str = "127.0.0.1";
+const ROUTING_UPDATE_INTERVAL: u64 = 15;
 
 pub type ConnectionsTableType = Arc<Mutex<HashMap<String, TcpStream>>>;
 pub type RoutingTableType = Arc<Mutex<Routingtable>>;
@@ -47,12 +48,12 @@ async fn main() {
     let tui = tui::Tui::new(morganite.clone());
 
     // Spawn connection handler
-    let connectionthread = tokio::spawn(async move {
+    let _connectionthread = tokio::spawn(async move {
         connection_handler.await.listen().await;
     });
     
     // Spawn tui
-    let tuithread = tokio::spawn(async move {
+    let _tuithread = tokio::spawn(async move {
         tui.handle_console().await;
     });
 
@@ -62,7 +63,12 @@ async fn main() {
     // Print routing table
     morganite.lock().await.print_routingtable().await;
 
+    loop {
+        tokio::time::sleep(Duration::from_secs(ROUTING_UPDATE_INTERVAL)).await;
+        morganite.lock().await.broadcast_routingtable().await;
+    }
+
     // Wait for exit
-    join!(connectionthread, tuithread).0.unwrap();
+    // join!(connectionthread, tuithread).0.unwrap();
 }
 
